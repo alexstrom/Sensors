0d9c8a0a2121097015da2bd485cc6f094ee72176
alexstrom <c15alex.strom@usafa.edu>
2013-12-06 15:18:49 -0700

    header file

---
+++ sensor.h
@@ -0,0 +1,17 @@
+/*
+ * sensor.h
+ *
+ *  Created on: Dec 4, 2013
+ *      Author: C15Alex.Strom
+ */
+
+#ifndef SENSOR_H_
+#define SENSOR_H_
+
+void initsensors();
+void middlesensor();
+void leftsensor();
+void rightsensor();
+
+
+#endif /* SENSOR_H_ */
